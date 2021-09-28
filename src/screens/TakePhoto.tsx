85fe1b54dae76f32a3ab84bfa89865dd1b08647e
SeongHoon Wee <mrgravity817@gmail.com>
2021-09-28 12:13:50 +0900

    Add two camera screens

---
+++ src/screens/TakePhoto.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const TakePhoto = () => {
+  return (
+    <View>
+      <Text>Take Photo</Text>
+    </View>
+  );
+};
