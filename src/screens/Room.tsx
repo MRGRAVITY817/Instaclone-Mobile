8677e0907ec2abce5c585d62cc74118d4f507f05
SeongHoon Wee <mrgravity817@gmail.com>
2021-10-18 18:16:52 +0900

    Add TextInput and Message display

--- src/screens/Room.tsx
+++ src/screens/Room.tsx
@@ -1,9 +1,11 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
-import { MessagesNavParamList } from "../navigators/MessagesNav";
+import { FlatList, KeyboardAvoidingView } from "react-native";
+import { MessagesNav, MessagesNavParamList } from "../navigators/MessagesNav";
 import { gql, useQuery } from "@apollo/client";
-import { SeeRooms } from "../__generated__/SeeRooms";
+import { ScreenLayout } from "../components/ScreenLayout";
+import { SeeRoom, SeeRoom_seeRoom_messages } from "../__generated__/SeeRoom";
+import styled from "styled-components/native";
 
 const ROOM_QUERY = gql`
   query SeeRoom($id: Int!) {
@@ -21,10 +23,27 @@ const ROOM_QUERY = gql`
   }
 `;
 
+const TextInput = styled.TextInput`
+  margin-bottom: 50px;
+  width: 90%;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 1000px;
+`;
+const MessageContainer = styled.View``;
+const Author = styled.Text``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+
 type RoomProps = StackScreenProps<MessagesNavParamList, "Room">;
 
 export const Room: React.FC<RoomProps> = ({ navigation, route }) => {
-  const { data } = useQuery<SeeRooms>(ROOM_QUERY, {
+  const { data, loading } = useQuery<SeeRoom>(ROOM_QUERY, {
     variables: { id: route.params.id },
   });
   useEffect(() => {
@@ -32,9 +51,37 @@ export const Room: React.FC<RoomProps> = ({ navigation, route }) => {
       title: `${route.params.talkingTo}`,
     });
   });
+  const renderItem = (message: SeeRoom_seeRoom_messages) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message.user.avatar + "" }} />
+        <Username>{message.user.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
+
   return (
-    <View>
-      <Text>Message List</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "black" }}
+      keyboardVerticalOffset={100}
+      behavior="height"
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          inverted
+          style={{ width: "100%" }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => message?.id + ""}
+          renderItem={({ item }) => item && renderItem(item)}
+        />
+        <TextInput
+          placeholderTextColor="rgba(0, 0, 0, 0.3)"
+          placeholder="Write a message..."
+          returnKeyLabel="Send Message"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 };
