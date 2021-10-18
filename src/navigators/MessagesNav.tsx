2f75c1c9ace073d82e381b0b1fcd92fce0e36cdf
SeongHoon Wee <mrgravity817@gmail.com>
2021-10-18 16:58:28 +0900

    Get message rooms from backend

--- src/navigators/MessagesNav.tsx
+++ src/navigators/MessagesNav.tsx
@@ -2,12 +2,26 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { Room } from "../screens/Room";
 import { Rooms } from "../screens/Rooms";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
 export const MessagesNav = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Rooms" component={Rooms} />
+    <Stack.Screen
+      options={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: "black",
+        },
+        headerBackImage: ({ tintColor }) => (
+          <Ionicons color={tintColor} name="chevron-down" size={28} />
+        ),
+      }}
+      name="Rooms"
+      component={Rooms}
+    />
     <Stack.Screen name="Room" component={Room} />
   </Stack.Navigator>
 );
--- src/screens/Rooms.tsx
+++ src/screens/Rooms.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { ROOM_FRAGMENT } from "../hooks/fragments";
+import styled from "styled-components/native";
+import { SeeRooms, SeeRooms_seeRooms } from "../__generated__/SeeRooms";
+import { ScreenLayout } from "../components/ScreenLayout";
+
+const SEE_ROOMS_QUERY = gql`
+  query SeeRooms {
+    seeRooms {
+      ...RoomParts
+    }
+  }
+  ${ROOM_FRAGMENT}
+`;
+
+const RoomContainer = styled.View`
+  background-color: black;
+`;
+
+const RoomText = styled.Text`
+  color: white;
+`;
 
 export const Rooms = () => {
+  const { data, loading } = useQuery<SeeRooms>(SEE_ROOMS_QUERY);
+  const renderItem = (room: SeeRooms_seeRooms | null) =>
+    room && (
+      <RoomContainer>
+        <RoomText>
+          {room.unreadTotal === 0
+            ? "Name of the other person"
+            : `${room.unreadTotal} messages.`}
+        </RoomText>
+      </RoomContainer>
+    );
+
   return (
-    <View>
-      <Text>Room List</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeRooms}
+        keyExtractor={(item) => "" + item?.id}
+        renderItem={({ item }) => renderItem(item)}
+      />
+    </ScreenLayout>
   );
 };
